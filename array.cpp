d977075af3571f902d677c43156bc6d3fa3c7c0e
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2020-10-16 00:04:47 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
-int main()
+int main()   //mainfunction
 {
-int marks[]={45,50,28,38};
+int marks[]={100,200,300,350};
 int mathsmarks[4];
 
 cout<<marks[0];
@@ -10,7 +10,7 @@ cout<<marks[1];
 cout<<marks[2];
 cout<<marks[3];
 
-for(int i=0; i<4; i++)
+for(int i=0; i<4; i++)  //loop execution
 {
     cout<<i<<marks[i];
 }
